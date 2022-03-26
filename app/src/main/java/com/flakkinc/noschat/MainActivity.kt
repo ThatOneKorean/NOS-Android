f1bbc2424f0c698d9090f85eb6142e1166283d1c
ThatOneKorean <thatonekorean21@gmail.com>
2022-03-25 23:37:58 -0600

    Added main display and updated server list display. Now displays a server icon (will be able to change in future)

--- app/src/main/java/com/flakkinc/noschat/MainActivity.kt
+++ app/src/main/java/com/flakkinc/noschat/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.flakkinc.noschat
 
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.flakkinc.noschat.databinding.ActivityMainBinding
+import com.google.android.gms.common.internal.Objects
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.squareup.picasso.Picasso
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -38,8 +40,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val serverMenu = binding.serverMenu
+        val serverMenuContainer = binding.serverMenuContainer
+        val sideMenu = binding.sideNav
         val settingsMenu = binding.SettingsLayout
         val settingsButton = binding.openSettingsButton
+        val closeSettingsButton = binding.closeSettingsMenuButton
 
         if(user === null) {
             startActivity(Intent(this@MainActivity, LoginActivity::class.java))
@@ -49,16 +54,40 @@ class MainActivity : AppCompatActivity() {
                     var serverIds: List<String>? = it.value as List<String>?
                     serverIds?.let {
                         for (id in serverIds) {
-                            val temp = CardView(this)
-                            val params = RelativeLayout.LayoutParams(
-                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                ViewGroup.LayoutParams.WRAP_CONTENT
-                            )
-                            temp.layoutParams = params
-                            val temp2 = TextView(this)
-                            temp2.text = id
-                            temp.addView(temp2)
-                            serverMenu.addView(temp)
+                            database.child("servers").child(id).child("name").get().addOnSuccessListener { it ->
+                                if(it.exists()) {
+                                    var info: String = it.value as String
+                                    val temp = LinearLayout(this)
+                                    temp.orientation = LinearLayout.HORIZONTAL
+                                    var params = RelativeLayout.LayoutParams(
+                                        ViewGroup.LayoutParams.MATCH_PARENT,
+                                        ViewGroup.LayoutParams.WRAP_CONTENT
+                                    )
+                                    temp.layoutParams = params
+
+                                    val serverPicContainer = CardView(this)
+
+                                    params = RelativeLayout.LayoutParams(50.dpToPixels(this), 50.dpToPixels(this))
+                                    serverPicContainer.layoutParams = params
+                                    serverPicContainer.radius = 37.5f.dpToPixels(this)
+
+                                    val serverPic = ImageView(this)
+
+                                    database.child("servers").child(id).child("serverPic").get().addOnSuccessListener { it2 ->
+                                        if(it2.exists()) {
+                                            Picasso.get().load(it2.value.toString()).into(serverPic)
+                                        }
+                                    }
+
+                                    serverPicContainer.addView(serverPic)
+                                    temp.addView(serverPicContainer)
+
+                                    val temp2 = TextView(this)
+                                    temp2.text = info
+                                    temp.addView(temp2)
+                                    serverMenu.addView(temp)
+                                }
+                            }
                         }
                     }
 
@@ -70,20 +99,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.menuButton.setOnClickListener {view ->
-            if(binding.sideNav.visibility == View.VISIBLE) {
-                binding.sideNav.visibility = View.INVISIBLE
+            if(sideMenu.visibility == View.VISIBLE) {
+                sideMenu.visibility = View.INVISIBLE
             } else {
-                binding.sideNav.visibility = View.VISIBLE
-                serverMenu.visibility = View.INVISIBLE
+                sideMenu.visibility = View.VISIBLE
+                serverMenuContainer.visibility = View.INVISIBLE
             }
         }
 
         binding.serverButton.setOnClickListener {view ->
-            if(serverMenu.visibility == View.VISIBLE) {
-                serverMenu.visibility = View.INVISIBLE
+            if(serverMenuContainer.visibility == View.VISIBLE) {
+                serverMenuContainer.visibility = View.INVISIBLE
             } else {
-                serverMenu.visibility = View.VISIBLE
-                binding.sideNav.visibility = View.INVISIBLE
+                serverMenuContainer.visibility = View.VISIBLE
+                sideMenu.visibility = View.INVISIBLE
             }
         }
 
@@ -107,7 +136,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         settingsButton.setOnClickListener {view ->
-            settingsMenu
+            settingsMenu.visibility = View.VISIBLE
+            sideMenu.visibility = View.INVISIBLE
+        }
+
+        closeSettingsButton.setOnClickListener {view ->
+            settingsMenu.visibility = View.GONE
         }
 
         val imageView = binding.profPic
@@ -116,3 +150,6 @@ class MainActivity : AppCompatActivity() {
         Picasso.get().load(imageURL).into(imageView)
     }
 }
+
+fun Int.dpToPixels(context: Context):Int = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), context.resources.displayMetrics).toInt()
+fun Float.dpToPixels(context: Context):Float = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), context.resources.displayMetrics)
